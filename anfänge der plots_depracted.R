9210cd0a20f9eae69dff5d4f963de3baaf985682
gesagraf <gesa.graf@posteo.de>
2023-12-06 17:49:58 +0100

    aufräumen

--- anfänge der plots_depracted.R
+++ anfänge der plots_depracted.R
@@ -1,103 +1,8 @@
-#### setup ####
-library(ggplot2)
-
-#### setting variables ####
-
-mu <- 150         # Population mean
-sd <- 15          # Population sd
-n <- 100          # sample size
-number <- 300     # number of samples
-mu_prior <- 130   # mittelwert der priori
-tau_prior <- 10   # sd der priori
-flache_priori <- TRUE #flache oder normalveteile priori
-
-# Anzahl der Klassen nach Sturges' Regel (für Histogram der einzelnen Stichprobe)
-num_classes <- ceiling(log2(n) + 1)
-# Anzahl der Klassen nach Sturges' Regel (für Histogram der SKV)
-num_classesSKV <- ceiling(log2(number) + 1)
-
-#### generating data ####
-
-# draws samples of "number" iterations
-samp_df <- sapply(1:number, FUN = function(i) {
-  samp <- rnorm(n = n, mean = mu, sd = sd)
-}
-)
-
-
-#### mean ####
-#colum wise
-estimators <- apply(samp_df, MARGIN = 2, mean)
-
-
-# over all mean
-mean_est <- mean(estimators)
-
-#### min/max ####
-# mean colum wise
-minmax <- sapply(1:ncol(samp_df), function(i) {
-  (max(samp_df[,i]) + min(samp_df[,i])) / 2
-})
-
-# over all mean
-mean_minmax <- mean(minmax)
-
-#### Bayes #####
-# calculate likelihood
-mu_hat <- seq(mu - 2 * sd, mu + 2 * sd, length.out = 200)
-
-# Prior
-calculate_prior <- function(flache_priori, mu_hat, mu_prior, tau_prior) {
-  if (flache_priori) {
-    return(rep(1/length(mu_hat), length(mu_hat)))
-  } else {
-    return(dnorm(mu_hat, mean = mu_prior, sd = tau_prior))
-  }
-}
-
-prior_dens <- calculate_prior(flache_priori, mu_hat, mu_prior, tau_prior)
-
-results <- as.data.frame(matrix(ncol = number, nrow = 200))#200 weil wir für 200 punkte die likelihood berechnen
-
-for (i in 1:number) {
-likelihood_function <- sapply(mu_hat, FUN = function(i_mu){
-  prod(dnorm(samp_df[,i], mean = i_mu, sd = sd(samp_df[,i])))#bei i 1 einsetzen für testi
-  })
-
-  #Normierung der Likelihood #ich glaube wir müssen das nicht normieren aber macht es trotzdem Sinn? nicht so kleine zahlen
-  den_like <- Bolstad::sintegral(mu_hat, likelihood_function)      # Normierungskonstante
-  likelihood_function_norm <- likelihood_function / den_like$value # normierte Likelihood
-
-  #calculate posteriori
-  posterior0 <- prior_dens * likelihood_function_norm
-
-  #Posteriori normieren#hier nochmal
-  den_post <- Bolstad::sintegral(mu_hat, posterior0)
-  posterior <- posterior0 / den_post$value
-
-results[[i]] <- posterior
-}
-
-# Index des Maximums in jeder Spalte finden
-index_maximum <- as.numeric(sapply(1:length(results), function(i) {
-  which.max(results[[i]])
-}))
-
-# Wert von 'x' für das Maximum von 'y' finden
-bayesWerte <- sapply(1:length(results), function(i) {
-  mu_hat[index_maximum[i]]
-})
-
-# over all mean Bayes
-mean_estBayes <- mean(bayesWerte)
-
-
 #### plotting Mean####
 # single Sample
 # setting specific sample
 specific <- 5
 
-### sturges rule anzahl der bins
 ggplot(NULL, aes(x = samp_df[ , specific])) +
   geom_histogram(fill = "lightgrey", color = "black", bins = num_classes) +
   geom_point(aes(x = mean(samp_df[ , specific]), y = 0), shape = 17, size = 4, colour = "darkgrey") +
@@ -149,8 +54,8 @@ specific <- 5
 
 ggplot(NULL, aes(x = samp_df[ , specific])) +
   geom_histogram(fill = "lightgrey", color = "black", bins = num_classes) +
-  geom_point(aes(x = mean(samp_df[ , specific]), y = 0), shape = 17, size = 4, colour = "darkgrey") +
-  geom_vline(xintercept = mean(samp_df[ , specific]), colour = "black", linetype = "dotted") +
+  geom_point(aes(x = minmax[specific], y = 0), shape = 17, size = 4, colour = "darkgrey") +
+  geom_vline(xintercept = minmax[specific], colour = "black", linetype = "dotted") +
 
   # Skalen, Theme, Labs etc.
   #   xlim(mu - 2 * sd, mu + 2 * sd) +        # x Achse festlegen
@@ -164,11 +69,11 @@ ggplot(NULL, aes(x = samp_df[ , specific])) +
 
 
 # SKV
-ggplot(NULL, aes(x = estimators)) +
+ggplot(NULL, aes(x = minmax)) +
   geom_histogram(fill = "lightgrey", color = "black", bins = num_classesSKV) +
 
   # every sample as triangle
-  geom_point(aes(x = estimators, y = 0, color = "estimators"), shape = 17, size = 4) +
+  geom_point(aes(x = minmax, y = 0, color = "estimators"), shape = 17, size = 4) +
 
   # mu
   geom_point(aes(x = mu, y = 2, colour = "mu"), shape = 17, size = 4) +
@@ -199,8 +104,8 @@ specific <- 5
 
 ggplot(NULL, aes(x = samp_df[ , specific])) +
   geom_histogram(fill = "lightgrey", color = "black", bins = num_classes) +
-  geom_point(aes(x = mean(samp_df[ , specific]), y = 0), shape = 17, size = 4, colour = "darkgrey") +
-  geom_vline(xintercept = mean(samp_df[ , specific]), colour = "black", linetype = "dotted") +
+  geom_point(aes(x = bayesWerte[specific], y = 0), shape = 17, size = 4, colour = "darkgrey") +
+  geom_vline(xintercept = bayesWerte[specific], colour = "black", linetype = "dotted") +
 
   # Skalen, Theme, Labs etc.
   #   xlim(mu - 2 * sd, mu + 2 * sd) +        # x Achse festlegen
@@ -214,11 +119,11 @@ ggplot(NULL, aes(x = samp_df[ , specific])) +
 
 
 # SKV
-ggplot(NULL, aes(x = estimators)) +
+ggplot(NULL, aes(x = bayesWerte)) +
   geom_histogram(fill = "lightgrey", color = "black", bins = num_classesSKV) +
 
   # every sample as triangle
-  geom_point(aes(x = estimators, y = 0, color = "estimators"), shape = 17, size = 4) +
+  geom_point(aes(x = bayesWerte, y = 0, color = "estimators"), shape = 17, size = 4) +
 
   # mu
   geom_point(aes(x = mu, y = 2, colour = "mu"), shape = 17, size = 4) +
@@ -239,6 +144,58 @@ ggplot(NULL, aes(x = estimators)) +
     colour = NULL) +
   scale_color_manual(values = c(mu = "red", mean_estBayes = "black", estimators = "darkgrey"),
                      labels = c(mu = expression(mu), mean_estBayes = "Mean aller Bayesschätzer",
+                                estimators = "Stichprobenmittelwerte")) +
+  theme_bw()
+
+
+#### plotting Bayes####
+
+# single Sample
+# setting specific sample
+specific <- 5
+
+ggplot(NULL, aes(x = samp_df[ , specific])) +
+  geom_histogram(fill = "lightgrey", color = "black", bins = num_classes) +
+  geom_point(aes(x = bayesWerteNV[specific], y = 0), shape = 17, size = 4, colour = "darkgrey") +
+  geom_vline(xintercept = bayesWerteNV[specific], colour = "black", linetype = "dotted") +
+
+  # Skalen, Theme, Labs etc.
+  #   xlim(mu - 2 * sd, mu + 2 * sd) +        # x Achse festlegen
+  labs(
+    title = "Einzelne Stichprobe mit Mittelwert",
+    y = "Häufigkeit",
+    x = "x",
+    labs = "blibla"
+  ) +
+  theme_bw()
+
+
+# SKV
+ggplot(NULL, aes(x = bayesWerteNV)) +
+  geom_histogram(fill = "lightgrey", color = "black", bins = num_classesSKV) +
+
+  # every sample as triangle
+  geom_point(aes(x = bayesWerteNV, y = 0, color = "estimators"), shape = 17, size = 4) +
+
+  # mu
+  geom_point(aes(x = mu, y = 2, colour = "mu"), shape = 17, size = 4) +
+  geom_vline(aes(xintercept = mu, colour = "mu")) +
+
+  # mean over all samples
+  geom_point(aes(x = mean_estBayesNV, y = 4,  colour = "mean_estBayes"), shape = 17, size = 4) +
+  geom_vline(aes(xintercept = mean_estBayesNV, colour = "mean_estBayes"), linetype = "dashed") +
+
+
+  # Skalen, Theme, Labs etc.
+  #  xlim(mu - sd, mu + sd) +  # xAchse festlegen
+  # coord_cartesian()
+  labs(
+    title = "Stichprobenkennwerteverteilung \n des arithmetischen Mittels mit mitlerem Bayesschätzer",
+    y = "Häufigkeit",
+    x = expression(bar(x)),
+    colour = NULL) +
+  scale_color_manual(values = c(mu = "red", mean_estBayesNV = "black", estimators = "darkgrey"),
+                     labels = c(mu = expression(mu), mean_estBayesNV = "Mean aller Bayesschätzer",
                                 estimators = "Stichprobenmittelwerte")) +
   theme_bw()
 
--- notiz_binwidt.R
+++
@@ -1,2 +0,0 @@
-geom_histogram(fill = "lightgrey", binwidth = ((max(samp_df[ , specific]) - min(samp_df[ , specific])) / num_classes)) +
-
