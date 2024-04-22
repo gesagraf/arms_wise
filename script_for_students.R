d8cf6bf4a6e2f6223fef6b0d52e4b006daa91260
Constantin-W <115614560+Constantin-W@users.noreply.github.com>
2024-04-22 17:00:44 +0200

    initial commit

---
+++ script_for_students.R
@@ -0,0 +1,11 @@
+if(!require(viridis))install.packages("viridis");library(viridis)
+if(!require(ggplot2))install.packages("ggplot2");library(ggplot2)
+if(!require(grid))install.packages("grid");library(grid)
+if(!require(gridExtra))install.packages("gridExtra");library(gridExtra)
+if(!require(cowplot))install.packages("cowplot");library(cowplot)
+if(!require(latex2exp))install.packages("latex2exp");library(latex2exp)
+if(!require(shinycssloaders))install.packages("shinycssloaders");library(shinycssloaders)
+if(!require(Bolstad))install.packages("Bolstad");library(Bolstad)
+library(shiny)
+
+runGitHub("arms_wise", "gesagraf")
