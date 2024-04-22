9279ce3cacdb76f6c37cd7df86f2cd85cdbb4efd
Constantin-W <115614560+Constantin-W@users.noreply.github.com>
2024-04-22 16:55:37 +0200

    added installation of packages if necessary

--- server.R
+++ server.R
@@ -2,12 +2,15 @@ server <- function(input, output, session) {
 
   # browser() #debug
   # setup
-  library(viridis)
-  library(ggplot2)
-  library(grid)
-  library(gridExtra)
-  library(cowplot)
-  library(latex2exp)
+  if(!require(viridis))install.packages("viridis");library(viridis)
+  if(!require(ggplot2))install.packages("ggplot2");library(ggplot2)
+  if(!require(grid))install.packages("grid");library(grid)
+  if(!require(gridExtra))install.packages("gridExtra");library(gridExtra)
+  if(!require(cowplot))install.packages("cowplot");library(cowplot)
+  if(!require(latex2exp))install.packages("latex2exp");library(latex2exp)
+  if(!require(shinycssloaders))install.packages("shinycssloaders");library(shinycssloaders)
+  if(!require(Bolstad))install.packages("Bolstad");library(Bolstad)
+
 
   set.seed(12345)
 
